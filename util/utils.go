23243be7b46686f984ef3e220ddb09864d16865d
dtop <dantop912@gmail.com>
2021-08-07 10:22:39 +0200

    Fixed mispelling that was breaking build

--- util/utils.go
+++ util/utils.go
@@ -70,7 +70,7 @@ func ReadConfig(configName string) (config configStruct) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	confDif = filepath.Join(confDir,"goblocks")
+	confDir = filepath.Join(confDir,"goblocks")
 
 	var file *os.File
 	file, err = os.Open(filepath.Join(confDir,configName))
